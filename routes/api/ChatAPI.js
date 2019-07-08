5b1db799ee71a1cf1e52378dcadc2c33dc205a30
Alex <alex.kreinis@walla.co.il>
2019-07-09 01:49:06 +0300

    first commit

---
+++ routes/api/ChatAPI.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const Chat = require('../../models/Chat');
+
+router.get('/:id/:otherid', async (req, res) => {
+  try {
+    const chat1 = await Chat.findOne({
+      otherID: req.params.otherid,
+      ID: req.params.id
+    });
+    const chat2 = await Chat.findOne({
+      otherID: req.params.id,
+      ID: req.params.otherid
+    });
+    if (chat1) {
+      res.json(chat1);
+    } else if (chat2) {
+      res.json(chat2);
+    } else {
+      res.json(null);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.post('/', async (req, res) => {
+  const chat1 = await Chat.findOne({
+    otherID: req.body.otherID,
+    ID: req.body.ID
+  });
+  const chat2 = await Chat.findOne({
+    otherID: req.body.ID,
+    ID: req.body.otherID
+  });
+  if (chat1) {
+    req.body.chat.map(chat => {
+      chat.text = encrypt(chat.text);
+    });
+    chat1.chat = [...req.body.chat];
+    chat1.save();
+  } else if (chat2) {
+    chat2.chat = [...req.body.chat];
+    chat2.save();
+  } else {
+    const newChat = new Chat({
+      ID: req.body.ID,
+      otherID: req.body.otherID,
+      chat: req.body.chat
+    });
+    newChat
+      .save()
+      .then(post => res.json(post))
+      .catch(err => console.log(err));
+  }
+});
+router.delete('/:id', (req, res) => {
+  Profile.findOne({ user: req.user.id }).then(profile => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notauthorized: 'User not authorized' });
+        }
+
+        post.remove().then(() => res.json({ success: true }));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  });
+});
+
+module.exports = router;
