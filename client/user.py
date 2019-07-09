484d273562f5dd2e79dc513dd18020ee2d7961ad
matan <matanda1@ac.sce.ac.il>
2019-07-09 18:42:44 +0300

    add : send msg method to user

--- client/user.py
+++ client/user.py
@@ -1,4 +1,6 @@
+import requests
 class User():
+    URL = "http://LocalHosr:15888/api/chat"
     def __init__(self,id,name,lastName,ip,motherBoard,password,friendsList):
         self.id = id
         self.name = name
@@ -9,7 +11,10 @@ class User():
         self.motherBoard = motherBoard
         self.friendsList = friendsList
     def sendMessage(self,msg,friend_id):
-        pass
+        PARAMS = {'ID': id, "otherID": friend_id, 'chat': msg}
+        r = requests.post(url=User.URL, params=PARAMS)
+        data = r.json()
+
     def getMessage(self,friend_id):
         pass
     def getFriends(self):
