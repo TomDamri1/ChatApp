52a02e38cd1595683779e4882f3df5508cccb885
matan <matanda1@ac.sce.ac.il>
2019-07-09 17:07:43 +0300

    new two classes : user && message

---
+++ client/user.py
@@ -0,0 +1,26 @@
+class User():
+    def __init__(self,id,name,lastName,ip,motherBoard,password,friendsList):
+        self.id = id
+        self.name = name
+        self.lastName = lastName
+        self.ip = ip
+        self.motherBoard = motherBoard
+        self.password = password
+        self.motherBoard = motherBoard
+        self.friendsList = friendsList
+    def sendMessage(self,msg,friend_id):
+        pass
+    def getMessage(self,friend_id):
+        pass
+    def getFriends(self):
+        pass
+    def approveControl(self):
+        pass
+    def getControl(self,friend_id):
+        pass
+    def getmotherBoard(self,friend_id):
+        pass
+    def addFriend(self, friend_id):
+        pass
+    def deleteFriend(self, friend_id):
+        pass
