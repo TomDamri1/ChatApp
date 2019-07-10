546965d5c73bb8297ee00a8edc7d98b3b74da7f7
matan <matanda1@ac.sce.ac.il>
2019-07-11 01:38:50 +0300

    Merge branch 'master' of https://github.com/AlexKreinis/ChatApp
    # Conflicts:
    #	.idea/ChatApp.iml
    #	.idea/misc.xml

--- client/user.py
+++ client/user.py
@@ -1,7 +1,11 @@
 import requests
-class User():
-    URL = "http://LocalHosr:15888/api/chat"
-    def __init__(self,id,name,lastName,ip,motherBoard,password,friendsList):
+
+
+class User:
+    postURL = "http://localhost:5000/api/chat"
+    getURL= "http://localhost:5000/api/chat/"
+
+    def __init__(self, id, name, lastName, ip, motherBoard, password, friendsList):
         self.id = id
         self.name = name
         self.lastName = lastName
@@ -10,22 +14,48 @@ class User():
         self.password = password
         self.motherBoard = motherBoard
         self.friendsList = friendsList
-    def sendMessage(self,msg,friend_id):
-        PARAMS = {'ID': id, "otherID": friend_id, 'chat': msg}
-        r = requests.post(url=User.URL, params=PARAMS)
-        data = r.json()
+
+    def sendMessage(self, msg, friend_id):
+        if friend_id in self.friendsList:
+            PARAMS = {'ID': self.id, "otherID": friend_id, 'chat': [{"senderName": self.name, "text": msg}, ]}
+            r = requests.post(url=User.postURL, json=PARAMS)
+            pastebin_url = r.text
+            print(pastebin_url)
+        else:
+            print("ERROR can't to send a message to friend that not in your's friendsList")
 
     def getMessage(self, friend_id):
-        pass
+        # Params : friend ID - the id of the friend that the message will be send to.
+        # return the content of the message
+        curURL = User.getURL + "{}/{}".format(self.id, friend_id)
+        r = requests.get(url=curURL)
+        data = r.json()
+        text = data['chat'][0]["text"]
+        return text
+
     def getFriends(self):
-        pass
+        return self.friendsList
+
     def approveControl(self):
         pass
+
     def getControl(self,friend_id):
         pass
+
     def getmotherBoard(self,friend_id):
         pass
+
     def addFriend(self, friend_id):
-        pass
+        self.friendsList.append(friend_id)
+
     def deleteFriend(self, friend_id):
-        pass
+        try:
+            self.friendsList.remove(friend_id)
+        except ValueError as e:
+            print(e)
+
+
+us1 = User('205509', 'matan', 'davidian', '0.0.0.0', 'intel mother Board', '123123', ['2312', '12332', '123'])
+us1.sendMessage("hello my name is matan third try", '123')
+print(us1.getMessage('123'))
+
