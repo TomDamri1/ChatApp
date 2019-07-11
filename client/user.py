85679d8767017ee9b230de15bcd6a7e2be5e0bcb
matan <matanda1@ac.sce.ac.il>
2019-07-11 18:38:46 +0300

    Add : get Mother Board by using terminal as sudo

--- client/user.py
+++ client/user.py
@@ -1,6 +1,7 @@
 import requests
-
-
+import os
+import subprocess
+import time
 class User:
     postURL = "http://localhost:5000/api/chat"
     getURL= "http://localhost:5000/api/chat/"
@@ -39,23 +40,43 @@ class User:
     def approveControl(self):
         pass
 
-    def getControl(self,friend_id):
+    def getControl(self, friend_id):
         pass
 
-    def getmotherBoard(self,friend_id):
-        pass
+    def findMotherBoard(self):
+        #return the name of the motherboard by using bash
+        sudoPassword = '1212'
+        command = 'dmidecode -t baseboard'
+        motherBoardManufacturer = subprocess.check_output('%s|sudo -S %s | grep Manufacturer &>/dev/null' % (sudoPassword, command), shell=True)
+        prodNmane = '\'Product Name\''
+        motherBoardProductName = subprocess.check_output('%s|sudo -S %s | grep %s &>/dev/null' % (sudoPassword, command, prodNmane), shell=True)
+        return motherBoardManufacturer.decode("utf-8") + motherBoardProductName.decode("utf-8")
+
+    def setmotherBoard(self):
+        self.motherBoard = self.findMotherBoard()
+
+    def getmotherBoard(self, friend_id):
+        return self.motherBoard
 
     def addFriend(self, friend_id):
         self.friendsList.append(friend_id)
+        # need to add the friend to the friends data base
 
     def deleteFriend(self, friend_id):
+        # Param : friend_id that need to be deleted from the friend list
         try:
             self.friendsList.remove(friend_id)
+            # need to delete the friend from the friends data base
         except ValueError as e:
             print(e)
 
+    def connect(self):
+        # pull the friend list and my derails from the server
+        pass
+
 
 us1 = User('205509', 'matan', 'davidian', '0.0.0.0', 'intel mother Board', '123123', ['2312', '12332', '123'])
-us1.sendMessage("hello my name is matan third try", '123')
-print(us1.getMessage('123'))
+print(us1.findMotherBoard())
+#us1.sendMessage("hello my name is matan third try", '123')
+#print(us1.findMotherBoard('123'))
 
