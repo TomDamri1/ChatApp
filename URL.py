9fcfcf29a1f5446b730c7ebaa7afb72c75069fa2
matan <matanda1@ac.sce.ac.il>
2019-07-23 17:11:54 +0300

    Merge branch 'master' of /home/matan/Desktop/ChatApp with conflicts.

--- URL.py
+++ URL.py
@@ -1,5 +1,5 @@
-URL = "http://linuxchat.herokuapp.com/"
-#URL = "http://localhost:5000/"
+#URL = "http://linuxchat.herokuapp.com/"
+URL = "http://localhost:5000/"
 postURL = URL + "api/chat/"
 getURL = URL + "api/chat/"
 usersURL = URL + "api/users/"
