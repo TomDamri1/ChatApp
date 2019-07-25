bdb80f8d976178082010a3f2e063c53781f7436b
matan <matanda1@ac.sce.ac.il>
2019-07-25 21:21:52 +0300

    default online server

--- URL.py
+++ URL.py
@@ -1,16 +1,16 @@
 from time import sleep
 try:
-    print('try')
+    # print('try')
     sleep(0.1)
     f = open("../../url.txt", 'r')
     if f.mode == 'r':
         URL = f.read()
     f.close()
 except Exception as error:
-    print('except')
+    # print('except')
     print(error)
     URL = "http://linuxchat.herokuapp.com/"
-print('URL is:' + URL)
+# print('URL is:' + URL)
 '''
 URL = "http://linuxchat.herokuapp.com/"
 URL2 = "http://localhost:5000/"
